fa86c20ee60ae7e9a72d7f23c87cc60dfc14fcfc
Jack <tvngoc1@cmc.com.vn>
2021-11-04 11:49:34 +0700

    update share

--- App.js
+++ App.js
@@ -6,127 +6,143 @@
  * @flow strict-local
  */
 
-import React, {useRef, useState} from 'react';
-import type {Node} from 'react';
-import {
-  Alert,
-  Button,
-  Platform,
-  TextInput,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
+ import React, {useState} from 'react';
+ import {
+   Alert,
+   Button,
+   Platform,
+   TextInput,
+   StyleSheet,
+   Text,
+   View,
+ } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import Share from 'react-native-share';
-import ViewShot from 'react-native-view-shot';
-import ImgToBase64 from 'react-native-image-base64';
-const App: () => Node = () => {
-  const refViewShot = useRef('initialValue');
-  const [urlImage, setUrlImage] = useState('');
+ import Share from 'react-native-share';
 
-  const screenShot = async () => {
-    console.log('refViewShot', refViewShot.current);
+ import images from './images/imagesBase64';
+ import pdfBase64 from './images/pdfBase64';
+
+ const App = () => {
+   const [packageSearch, setPackageSearch] = useState<string>('');
+   const [recipient, setRecipient] = useState<string>('');
+   const [result, setResult] = useState<string>('');
+
+   /**
+    * You can use the method isPackageInstalled to find if a package is installed.
+    * It returns an object { isInstalled, message }.
+    * Only works on Android.
+    */
+   const checkIfPackageIsInstalled = async () => {
+     const {isInstalled} = await Share.isPackageInstalled(packageSearch);
+
+     Alert.alert(
+       `Package: ${packageSearch}`,
+       `${isInstalled ? 'Installed' : 'Not Installed'}`,
+     );
+   };
+
+   function getErrorString(error, defaultValue) {
+     let e = defaultValue || 'Something went wrong. Please try again';
+     if (typeof error === 'string') {
+       e = error;
+     } else if (error && error.message) {
+       e = error.message;
+     } else if (error && error.props) {
+       e = error.props;
+     }
+     return e;
+   }
+
+
+   const shareEmailImage = async () => {
+     const shareOptions = {
+       title: 'Share file',
+       email: 'letrungdong@gmail.com.vn',
+       social: Share.Social.EMAIL,
+       failOnCancel: false,
+       message:"test",
+       url: images.image1,
+     };
+
+     try {
+       const ShareResponse = await Share.shareSingle(shareOptions);
+       setResult(JSON.stringify(ShareResponse, null, 2));
+     } catch (error) {
+       console.log('Error =>', error);
+       setResult('error: '.concat(getErrorString(error)));
+     }
+   };
+   const shareWhatsapp = async () => {
+    const shareOptions = {
+      title: 'Share file',
+      social: Share.Social.WHATSAPP,
+      failOnCancel: false,
+      message:"test",
+      url: images.image1,
+      whatsAppNumber: "0084329563942" // country code + phone number
+    };
 
     try {
-      let url = await refViewShot.current.capture();
-      console.log('irl', url);
-      let imageBase64 = await ImgToBase64.getBase64String(url);
-      alert('Screenshot is success.');
-      setUrlImage(imageBase64);
+      const ShareResponse = await Share.shareSingle(shareOptions);
+      setResult(JSON.stringify(ShareResponse, null, 2));
     } catch (error) {
-      alert(error);
+      console.log('Error =>', error);
+      setResult('error: '.concat(getErrorString(error)));
     }
   };
+   return (
+     <View style={styles.container}>
+       <Text style={styles.welcome}>Welcome to React Native Share Example!</Text>
+       <View style={styles.optionsRow}>
+         <View style={styles.button}>
+           <Button onPress={shareEmailImage} title="Share Social: Email" />
+         </View>
+         <View style={styles.button}>
+           <Button onPress={shareWhatsapp} title="Share Social: Whatsapp" />
+         </View>
+         <Text style={styles.resultTitle}>Result</Text>
+         <Text style={styles.result}>{result}</Text>
+       </View>
+     </View>
+   );
+ };
 
-  const shareScreen = async () => {
-    if (!urlImage) {
-      alert(`you didn't capture the screen`);
-      return
-    }
-    // console.log(urlImage, 'urlImage');
-    try {
-      const shareOption = {
-        url: 'data:image/png;base64,' + urlImage,
-        filename: 'test.png',
-        type: 'image/png',
-      };
+ const styles = StyleSheet.create({
+   button: {
+     marginBottom: 10,
+   },
+   container: {
+     flex: 1,
+     justifyContent: 'center',
+     alignItems: 'center',
+     backgroundColor: '#F5FCFF',
+   },
+   textInput: {
+     borderBottomColor: '#151313',
+     borderBottomWidth: 1,
+     marginRight: 10,
+   },
+   welcome: {
+     fontSize: 20,
+     textAlign: 'center',
+     margin: 10,
+   },
+   resultTitle: {
+     marginTop: 20,
+     fontSize: 20,
+   },
+   result: {
+     fontSize: 14,
+     margin: 10,
+   },
+   optionsRow: {
+     justifyContent: 'space-between',
+   },
+   withInputContainer: {
+     alignItems: 'center',
+     justifyContent: 'center',
+     flexDirection: 'row',
+   },
+ });
 
-      const responseShare = await Share.open(shareOption);
-      console.log('responseShare', JSON.stringify(responseShare));
-    } catch (error) {
-      console.log('errror', error);
-    }
-  };
-  const shareText = async () => {
-    try {
-      const shareOption = {
-        message: 'Hellooo',
-      };
-
-      const responseShare = await Share.open(shareOption);
-      console.log('responseShare', JSON.stringify(responseShare));
-    } catch (error) {
-      console.log('errror', error);
-    }
-  };
-  return (
-    <ViewShot
-      ref={refViewShot}
-      options={{format: 'png', quality: 0.9}}
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-      }}>
-      <TouchableOpacity
-        onPress={screenShot}
-        style={{
-          backgroundColor: 'blue',
-          width: 100,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 5,
-        }}>
-        <Text style={{color: 'white'}}>Screenshot</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={shareText}
-        style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 5, marginTop: 30,
-        }}>
-        <Text style={{color: 'white'}}>Share Text: Hellooo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={shareScreen}
-        style={{
-          backgroundColor: 'green',
-          width: 100,
-          height: 50,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 5,
-          marginTop: 30,
-        }}>
-        <Text style={{color: 'white'}}>Share image screenshort</Text>
-      </TouchableOpacity>
-    </ViewShot>
-  );
-};
-
-export default App;
+ export default App;
